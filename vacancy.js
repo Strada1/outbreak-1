6202e34932268cdc37e2a5d054efc65701472f44
Nikita <ifyoucan1337@yandex.ru>
2023-02-21 01:23:23 +0300

    add vacancy

---
+++ vacancy.js
@@ -0,0 +1,208 @@
+const vacancy = [
+	{
+		company: 'FINCH.',
+		vacancyLink:
+			'https://hh.ru/vacancy/76160335?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: 'Next.js, Graphql + Apollo',
+		status: 'active',
+		lastUpdate: '17.02.23',
+	},
+	{
+		company: 'СберКорус',
+		vacancyLink:
+			'https://hh.ru/vacancy/76865782?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: 'SOAP, Headless CMS, AGILE, DOcker',
+		status: 'active',
+		lastUpdate: '16.02.23',
+	},
+	{
+		company: 'Lesta Games',
+		vacancyLink:
+			'https://hh.ru/vacancy/76681184?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: '2D, Web Develompent',
+		status: 'active',
+		lastUpdate: '18.02.23',
+	},
+	{
+		company: 'НКО Монета (ООО)',
+		vacancyLink:
+			'https://hh.ru/vacancy/76325523?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge:
+			'ООП, DevOps практики (выстраивание непрерывного процесса доставки продукта с автоматизированными критериями приёмки)',
+		status: 'active',
+		lastUpdate: '17.02.23',
+	},
+	{
+		company: 'NovaKid ',
+		vacancyLink:
+			'https://hh.ru/vacancy/77176534?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '19.02.23',
+	},
+	{
+		company: 'Express-Шина',
+		vacancyLink:
+			'https://hh.ru/vacancy/76755326?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge:
+			'Webpack, модульное тестирование, SEO, паттерны-проектирования',
+		status: 'active',
+		lastUpdate: '20.02.23',
+	},
+	{
+		company: 'Тэсонэро',
+		vacancyLink:
+			'https://hh.ru/vacancy/76719111?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: 'Vue.js,',
+		status: 'active',
+		lastUpdate: '19.02.23',
+	},
+	{
+		company: 'ООО Тракетория',
+		vacancyLink:
+			'https://hh.ru/vacancy/76608438?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '18.02.23',
+	},
+	{
+		company: 'ЗАО Норси-Транс',
+		vacancyLink:
+			'https://hh.ru/vacancy/75887812?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: 'PostgreSQL',
+		status: 'active',
+		lastUpdate: '19.02.23',
+	},
+	{
+		company: 'ModelTech',
+		vacancyLink:
+			'https://hh.ru/vacancy/76151326?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '17.02.23',
+	},
+	{
+		company: 'ООО Итология',
+		vacancyLink:
+			'https://hh.ru/vacancy/76248542?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: 'опыт написания тестов Jest, Enzyme',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'iSpring ',
+		vacancyLink:
+			'https://hh.ru/vacancy/77068633?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'АО КрымТехнологии',
+		vacancyLink:
+			'https://hh.ru/vacancy/76382375?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'LoyaltyLabs',
+		vacancyLink:
+			'https://hh.ru/vacancy/77084827?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA',
+		missingKnoweledge: 'клиент-серверной архитектура, написание unit-тестов',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'Сбер',
+		vacancyLink:
+			'https://hh.ru/vacancy/74081795?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge:
+			'react-hook-form or emotion, Unit тесты на Jest, Docker, Kafka, Atlassian',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'ООО Ингруппа',
+		vacancyLink:
+			'https://hh.ru/vacancy/76493206?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: 'NodeJS/NestJS',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'ООО Пилигрим',
+		vacancyLink:
+			'https://hh.ru/vacancy/77178106?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: 'Turf.js',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'БКС IT & Digital',
+		vacancyLink:
+			'https://hh.ru/vacancy/76978541?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: 'Next.js(будет плюсом)',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'UP.ru',
+		vacancyLink:
+			'https://hh.ru/vacancy/76913227?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: 'ООП',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'more.tv',
+		vacancyLink:
+			'https://hh.ru/vacancy/72930561?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge:
+			'Web API (Intersection Observer, Workers, IndexedDB, History), Глубокие знания протокола HTTP, понимание принципов работы браузерного и сетевого кеша, умение сформировать и проверить ручными средствами нестандартный запрос (дополнительные заголовки, загрузка файлов, json body, cache busting), Уверенные навыки автоматического тестирования (unit, snapshot), представление о E2E-тестировании, Docker',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'ААМ Системз',
+		vacancyLink:
+			'https://hh.ru/vacancy/76818426?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'DIGITAL SECTOR',
+		vacancyLink:
+			'https://hh.ru/vacancy/71558942?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'Городские информационные системы',
+		vacancyLink:
+			'https://hh.ru/vacancy/76753903?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: 'принципы ООП, тесты(интеграционные, сценария)',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'Unicorn',
+		vacancyLink:
+			'https://hh.ru/vacancy/76771735?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '',
+	},
+	{
+		company: 'Lazurit',
+		vacancyLink:
+			'https://hh.ru/vacancy/76703546?from=vacancy_search_list&query=Frontend%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20react',
+		missingKnoweledge: '-',
+		status: 'active',
+		lastUpdate: '',
+	},
+]
+
+const json = JSON.stringify(vacancy)
